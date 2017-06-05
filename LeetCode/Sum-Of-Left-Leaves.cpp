31d8c7a33d55c188e481e000da000b6ff19c106c
James Le <le_j6@denison.edu>
2017-06-05 12:12:18 -0700

    Sum Of Left Leaves

---
+++ LeetCode/Sum-Of-Left-Leaves.cpp
@@ -0,0 +1,21 @@
+// Find the sum of all left leaves in a given binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(!root) return 0;
+        if(root->left && !root->left->left && !root->left->right) {
+            return root->left->val + sumOfLeftLeaves(root->right);
+        }
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
