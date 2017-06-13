2c0c4de4741142c2505afd34b6b66f4e9e7921ab
James Le <le_j6@denison.edu>
2017-06-13 13:13:25 -0700

    Perfect Squares

---
+++ LeetCode/Perfect-Squares.cpp
@@ -0,0 +1,67 @@
+// Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+// For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+
+// Dynamic Programming Solution
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+
+        // cntPerfectSquares[i] = the least number of perfect square numbers
+        // which sum to i. Note that cntPerfectSquares[0] is 0.
+        vector<int> cntPerfectSquares(n + 1, INT_MAX);
+        cntPerfectSquares[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            // For each i, it must be the sum of some number (i - j*j) and
+            // a perfect square number (j*j).
+            for (int j = 1; j*j <= i; j++)
+            {
+                cntPerfectSquares[i] =
+                    min(cntPerfectSquares[i], cntPerfectSquares[i - j*j] + 1);
+            }
+        }
+
+        return cntPerfectSquares.back();
+    }
+};
+
+// Static Dynamic Programming Solution
+class Solution
+{
+public:
+    int numSquares(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+
+        // cntPerfectSquares[i] = the least number of perfect square numbers
+        // which sum to i. Since cntPerfectSquares is a static vector, if
+        // cntPerfectSquares.size() > n, we have already calculated the result
+        // during previous function calls and we can just return the result now.
+        static vector<int> cntPerfectSquares({0});
+
+        // While cntPerfectSquares.size() <= n, we need to incrementally
+        // calculate the next result until we get the result for n.
+        while (cntPerfectSquares.size() <= n)
+        {
+            int m = cntPerfectSquares.size();
+            int cntSquares = INT_MAX;
+            for (int i = 1; i*i <= m; i++)
+            {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            }
+
+            cntPerfectSquares.push_back(cntSquares);
+        }
+
+        return cntPerfectSquares[n];
+    }
+};
