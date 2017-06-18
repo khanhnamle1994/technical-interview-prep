8dfb50fe9a94021e95f5c90a9e71a54227ca3472
James Le <le_j6@denison.edu>
2017-06-17 17:26:09 -0700

    Unique Characters In The String Revsied

--- Cracking-The-Coding-Interview/Arrays-and-Strings/1-1.cpp
+++ Cracking-The-Coding-Interview/Arrays-and-Strings/1-1.cpp
@@ -3,35 +3,49 @@
 
 #include <iostream>
 #include <cstring>
+#include <algorithm>
+#include <map>
 using namespace std;
 
-// If we assume that the character set is an ASCII character, then we can open a
-// bool array of size 256 to characterize the appearance of each character. The
-// array is intialized to false, traversing the characters in the string, and
-// when the value of the corresponding position of the bool array is true, indicating
-// that the character has already appeared before, it is possible to derive duplicate
-// characters in the string. Otherwise, set the value of the bool array to true.
-
-bool isUnique1(string s)
-{
-  bool a[256];
-  memset(a, 0, sizeof(a));
-  int len = s.length();
-  for(int i = 0; i < len; i++)
-  {
-    int v = (int)s[i];
-    if(a[v]) return false;
-    a[v] = true;
+// Brute-force solution O(n^2)
+bool isUnique(const string& s) {
+  for(int i = 0; i < s.length(); i++) {
+    for(int j = i + 1; j < s.length(); j++) {
+      if(s[i] == s[j]) {
+        return false;
+      }
+    }
   }
   return true;
 }
 
-// Time complexity of the algorithm is O(n)
+// Hash-table solution O(n)
+bool isUnique1(const string& s) {
+  map<char, bool> map;
+  for (int i = 0; i < s.length(); i++) {
+    if(map[s[i]]) {
+      return false;
+    } else {
+      map[s[i]] = true;
+    }
+  }
+  return true;
+}
+
+// No additional data structure O(n)
+bool isUnique2(string& s) {
+  std::sort(s.begin(), s.end());
+  for(int i = 0; i < s.length(); i++) {
+    if(s[i] == s[i - 1])
+      return false;
+  }
+  return true;
+}
 
 int main()
 {
   string s1 = "algorithm";
   string s2 = "alphabetical";
-  cout << isUnique1(s1) << " " << isUnique1(s2) << endl;
-  return 0; 
+  cout << isUnique2(s1) << " " << isUnique2(s2) << endl;
+  return 0;
 }
