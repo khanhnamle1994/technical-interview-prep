8746dd73498eeb031c2c47474dc76a97aa051e9e
James Le <le_j6@denison.edu>
2017-06-19 15:48:46 -0700

    Lowest Common Ancestor In Binary Tree

---
+++ LeetCode/Lowest-Common-Ancestor-Binary-Tree.cpp
@@ -0,0 +1,26 @@
+// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) { return NULL; }
+        if(root == p || root == q) { return root; }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left != NULL && right != NULL) { return root; }
+        if(left == NULL && right == NULL) { return NULL; }
+        return left != NULL ? left : right;
+    }
+};
+
+// O(log n) time complexity
