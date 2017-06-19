87e0663583d0d0c2f945740f18e185c4b0d5f67b
James Le <le_j6@denison.edu>
2017-06-18 20:41:25 -0700

    Delete Node From A Linked List Revised

--- Cracking-The-Coding-Interview/Linked-Lists/2-3.cpp
+++ Cracking-The-Coding-Interview/Linked-Lists/2-3.cpp
@@ -1,33 +1,32 @@
-// Implement an algorithm to delete a node in the middle of a singly linked list,
-// given only access to that node
+// Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node
 
 #include <iostream>
 using namespace std;
 
 typedef struct node{
-	int data;
-	node *next;
-}node;
+    int val;
+    node *next;
+} node;
 
 node* init(int a[], int n){
-	node *head, *p;
-	for(int i=0; i<n; ++i){
-		node *nd = new node();
-		nd->data = a[i];
-		if(i==0){
-			head = p = nd;
-			continue;
-		}
-		p->next = nd;
-		p = nd;
-	}
-	return head;
+    node *head, *p;
+    for(int i=0; i < n; ++i){
+        node *nd = new node();
+        nd->val = a[i];
+        if(i==0){
+            head = p = nd;
+            continue;
+        }
+        p->next = nd;
+        p = nd;
+    }
+    return head;
 }
 
-bool remove(node *c){
-	if(c==NULL || c->next==NULL) return false;
+bool remove(node *c) {
+	if(c == NULL || c->next == NULL) return false;
 	node *q = c->next;
-	c->data = q->data;
+	c->val = q->val;
 	c->next = q->next;
 	delete q;
 	return true;
@@ -35,23 +34,27 @@ bool remove(node *c){
 
 void print(node *head){
 	while(head){
-		cout<<head->data<<" ";
+		cout << head->val << " ";
 		head = head->next;
 	}
-	cout<<endl;
+	cout << endl;
 }
 
 int main(){
 	int n = 10;
 	int a[] = { 9, 2, 1, 3, 5, 6, 2, 6, 3, 1 };
 	node *head = init(a, n);
+
 	int cc = 3;
 	node *c = head;
-	for(int i=1; i<cc; ++i) c = c->next;
+
+	for(int i = 1; i < cc; ++i)
+		c = c->next;
 	print(head);
+
 	if(remove(c))
 		print(head);
 	else
-		cout<<"failure"<<endl;
+		cout << "failure" << endl;
 	return 0;
 }
