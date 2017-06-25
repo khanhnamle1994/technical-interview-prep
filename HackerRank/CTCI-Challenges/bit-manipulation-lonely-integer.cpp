8ce1740c58b223db745571f50128368b7e119a55
James Le <le_j6@denison.edu>
2017-06-25 14:21:13 -0700

    Lonely Integer

---
+++ HackerRank/CTCI-Challenges/bit-manipulation-lonely-integer.cpp
@@ -0,0 +1,48 @@
+// Consider an array of n integers, A = [a_{0}, a_{1}, ..., a{n - 1}], where all but one of the integers occur in pairs. In other words,
+// every element in A occurs exactly twice except for one unique element.
+
+// Given A, find and print the unique element.
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int lonely_integer(vector < int > a) {
+    int result = 0;
+    for(const auto& i : a) {
+        result ^= i;
+    }
+    return result;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[a_i];
+    }
+    cout << lonely_integer(a) << endl;
+    return 0;
+}
