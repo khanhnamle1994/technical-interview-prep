ea952bce53553dce4b9c8368eecf4b816a6331d0
James Le <le_j6@denison.edu>
2017-06-29 14:26:31 -0700

    Longest Substring Without Repeating Characters

---
+++ LeetCode/Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,26 @@
+// Given a string, find the length of the longest substring without repeating characters
+
+class Solution {
+public:
+  int lengthOfLongestSubstring(string s) {
+    if(s.length() < 2) return s.length();
+    int curlen = 1;
+    int maxlen = 1;
+    unordered_map<char, int> map;
+    map[s[0]] = 0;
+
+    for(int i = 1; i < s.length(); i++) {
+      if(map.count(s[i]) == 0 || map[s[i]] < i - curlen) {
+        curlen++;
+      } else {
+        curlen = i - map[s[i]];
+      }
+      map[s[i]] = i;
+      if(curlen > maxlen) {
+        maxlen = curlen;
+      }
+    }
+
+    return maxlen;
+  }
+}
