26dc5a05c2d063bdf63457d273144ccc80ccb177
James Le <le_j6@denison.edu>
2017-07-06 19:10:19 -0700

    Valid Parentheses

--- LeetCode/Valid-Parentheses.cpp
+++ LeetCode/Valid-Parentheses.cpp
@@ -1,2 +1,30 @@
 // Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 // The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+bool matching(char a, char b) {
+  if((a == '(') && b == ')') || (a == '{') && b == '}') || (a == '[') && b == ']')) {
+    return true;
+  }
+  return false;
+}
+
+bool isValid(string s) {
+  stack<int> result;
+  for(int i = 0; i < s.length(); i++) {
+    if(s[i] == '{' || s[i] == '[' || s[i] == '(') {
+      result.push(s[i]);
+    } else {
+      if(result.empty()) {
+        return false;
+      }
+      if(matching(result.top(), s[i])) {
+        result.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  if(result.empty()) { return true; }
+  else { return false; }
+}
