e42a371ea3a578798061a28e1f4061a4ff13b4ca
James Le <le_j6@denison.edu>
2017-10-08 07:59:02 +0700

    Isomorphic Strings

---
+++ LeetCode/Isomorphic-Strings.cpp
@@ -0,0 +1,21 @@
+// Given two strings s and t, determine if they are isomorphic.
+
+// Two strings are isomorphic if the characters in s can be replaced to get t.
+
+// All occurrences of a character must be replaced with another character while preserving the order of characters.
+// No two characters may map to the same character but a character may map to itself.
+
+/*For example,
+Given "egg", "add", return true.
+Given "foo", "bar", return false.
+Given "paper", "title", return true. */
+
+bool isIsomorphic(string s, string t) {
+    int m1[256] = {0}, m2[256] = {0}, n = s.size();
+    for (int i = 0; i < n; ++i) {
+        if (m1[s[i]] != m2[t[i]]) return false;
+        m1[s[i]] = i + 1;
+        m2[t[i]] = i + 1;
+    }
+    return true;
+}
