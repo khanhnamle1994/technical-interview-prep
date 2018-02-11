f64b4cd62391e3a21c544d33cfbc5504e3c515ba
James Le <le_j6@denison.edu>
2018-02-11 12:52:29 +0700

    Beautiful Arrangement

---
+++ LeetCode/Beautiful-Arrangement.cpp
@@ -0,0 +1,42 @@
+// Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed by these N numbers
+// successfully if one of the following is true for the ith position (1 <= i <= N) in this array:
+
+// 1. The number at the ith position is divisible by i.
+// 2. i is divisible by the number at the ith position.
+
+// Now given N, how many beautiful arrangements can you construct?
+
+/* Example 1:
+Input: 2
+Output: 2
+
+Explanation:
+
+The first beautiful arrangement is [1, 2]:
+Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+The second beautiful arrangement is [2, 1]:
+Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1. */
+
+int countArrangement(int N) {
+  vector<int> vs;
+  for (int i = 0; i < N; ++i) {
+    vs.push_back(i + 1);
+  }
+  return counts(N, vs);
+}
+
+int counts(int n, vector<int>& vs) {
+    if (n <= 0) return 1;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        if (vs[i] % n == 0 || n % vs[i] == 0) {
+            swap(vs[i], vs[n - 1]);
+            ans += counts(n - 1, vs);
+            swap(vs[i], vs[n - 1]);
+        }
+    }
+    return ans;
+}
