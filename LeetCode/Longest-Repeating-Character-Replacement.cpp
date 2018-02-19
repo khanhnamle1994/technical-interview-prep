ed1f7117fd4b93844e38107f9640100931a5631c
James Le <le_j6@denison.edu>
2018-02-19 21:49:25 +0700

    Longest Repeating Character Replacement

---
+++ LeetCode/Longest-Repeating-Character-Replacement.cpp
@@ -0,0 +1,32 @@
+// Given a string that consists of only uppercase English letters, you can replace any letter in the string with another letter at
+// most k times. Find the length of a longest substring containing all repeating letters you can get after performing the above
+// operations.
+
+/* Example 1:
+
+Input:
+s = "ABAB", k = 2
+Output:
+4
+Explanation:
+Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+
+Input:
+s = "AABABBA", k = 1
+Output:
+4
+Explanation:
+Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4. */
+
+int characterReplacement(string s, int k) {
+  int i = 0, j = 0, ctr[91] = {};
+  while (j < s.size()) {
+      ctr[s[j++]]++;
+      if (j-i - *max_element(ctr+65, ctr+91) > k)
+          ctr[s[i++]]--;
+  }
+  return j - i;
+}
