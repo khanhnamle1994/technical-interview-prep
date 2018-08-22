2681601207f0a31974db354573df6432494dca43
James Le <le_j6@denison.edu>
2018-08-21 22:12:51 -0400

    Search in a Binary Search Tree

---
+++ LeetCode/Search-in-a-BST.cpp
@@ -0,0 +1,37 @@
+// Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+/* For example,
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2
+     / \
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null. */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+TreeNode* searchBST(TreeNode* root, int val) {
+  while (root != nullptr && root->val != val) {
+    root = (root->val > val) ? root->left : root->right;
+  }
+  return root;
+}
