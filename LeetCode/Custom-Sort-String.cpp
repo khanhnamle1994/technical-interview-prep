4ba9d241d73a19fc2524944a7181a74604c63085
James Le <le_j6@denison.edu>
2018-11-10 12:00:59 -0500

    Custom Sort String

---
+++ LeetCode/Custom-Sort-String.cpp
@@ -0,0 +1,20 @@
+/* S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they match the order that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+
+Return any permutation of T (as a string) that satisfies this property.
+
+Example :
+Input:
+S = "cba"
+T = "abcd"
+Output: "cbad"
+Explanation:
+"a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a".
+Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs. */
+
+string customSortString(string S, string T) {
+  sort(T.begin(), T.end(),
+       [&](char a, char b) { return S.find(a) < S.find(b); });
+  return T;
+}
