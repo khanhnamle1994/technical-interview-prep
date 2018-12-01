efddc0c21b303062d5babb6920de5a2bc678717f
James Le <le_j6@denison.edu>
2018-12-01 17:06:19 -0500

    Max Chunks To Make Sorted

---
+++ LeetCode/Max-Chunks-to-Make-Sorted.cpp
@@ -0,0 +1,25 @@
+/* Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions), and individually sort each chunk.  After concatenating them, the result equals the sorted array.
+
+What is the most number of chunks we could have made?
+
+Example 1:
+Input: arr = [4,3,2,1,0]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+
+Example 2:
+Input: arr = [1,0,2,3,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [1, 0], [2, 3, 4].
+However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible. */
+
+int maxChunksToSorted(vector<int>& arr) {
+  for (auto i = 0, max_i = 0, ch = 0; i <= arr.size(); ++i) {
+    if (i == arr.size()) return ch;
+    max_i = max(max_i, arr[i]);
+    if (max_i == i) ++ch;
+  }
+}
