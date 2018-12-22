b6eeed5416f746d9ed6ebb679f67278201814877
James Le <le_j6@denison.edu>
2018-12-21 23:06:11 -0500

    Add One Row to Tree

---
+++ LeetCode/Add-One-Row-to-Tree.cpp
@@ -0,0 +1,70 @@
+/* Given the root of a binary tree, then value v and depth d, you need to add a row of nodes with value v at the given depth d. The root node is at depth 1.
+
+The adding rule is: given a positive integer depth d, for each NOT null tree nodes N in depth d-1, create two tree nodes with value v as N's left subtree root and right subtree root. And N's original left subtree should be the left subtree of the new left subtree root, its original right subtree should be the right subtree of the new right subtree root. If depth d is 1 that means there is no depth d-1 at all, then create a tree node with value v as the new root of the whole original tree, and the original tree is the new root's left subtree.
+
+Example 1:
+Input:
+A binary tree as following:
+       4
+     /   \
+    2     6
+   / \   /
+  3   1 5
+
+v = 1
+
+d = 2
+
+Output:
+       4
+      / \
+     1   1
+    /     \
+   2       6
+  / \     /
+ 3   1   5
+
+Example 2:
+Input:
+A binary tree as following:
+      4
+     /
+    2
+   / \
+  3   1
+
+v = 1
+
+d = 3
+
+Output:
+      4
+     /
+    2
+   / \
+  1   1
+ /     \
+3       1 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+TreeNode* addOneRow(TreeNode* root, int v, int d) {
+  if (d == 0 || d == 1) {
+      TreeNode* newroot = new TreeNode(v);
+      (d ? newroot->left : newroot->right) = root;
+      return newroot;
+  }
+  if (root && d >= 2) {
+      root->left  = addOneRow(root->left,  v, d > 2 ? d - 1 : 1);
+      root->right = addOneRow(root->right, v, d > 2 ? d - 1 : 0);
+  }
+  return root;
+}
