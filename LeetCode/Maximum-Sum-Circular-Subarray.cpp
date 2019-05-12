0aa8fa776c0aa10f685c0d1eb31420d04920df59
James Le <le_j6@denison.edu>
2019-05-11 22:26:09 -0400

    Maximum Sum Circular Subarray

---
+++ LeetCode/Maximum-Sum-Circular-Subarray.cpp
@@ -0,0 +1,42 @@
+/* Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
+
+Here, a circular array means the end of the array connects to the beginning of the array.  (Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+
+Also, a subarray may only include each element of the fixed buffer A at most once.  (Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+
+Example 1:
+Input: [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3
+
+Example 2:
+Input: [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
+
+Example 3:
+Input: [3,-1,2,-1]
+Output: 4
+Explanation: Subarray [2,-1,3] has maximum sum 2 + (-1) + 3 = 4
+
+Example 4:
+Input: [3,-2,2,-3]
+Output: 3
+Explanation: Subarray [3] and [3,-2,2] both have maximum sum 3
+
+Example 5:
+Input: [-2,-3,-1]
+Output: -1
+Explanation: Subarray [-1] has maximum sum -1 */
+
+int maxSubarraySumCircular(vector<int>& A) {
+    int total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
+    for (int a : A) {
+        curMax = max(curMax + a, a);
+        maxSum = max(maxSum, curMax);
+        curMin = min(curMin + a, a);
+        minSum = min(minSum, curMin);
+        total += a;
+    }
+    return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
+}
